5a14cc74de341bead0de787398a9b9f51aa41531
CheefOfficer <marksmaneld@gmail.com>
2024-11-19 00:33:09 +0300

    homework 4 done

---
+++ variables.py
@@ -0,0 +1,5 @@
+number_of_done_homework = 12
+spent_hours = 1.5
+name_of_course = 'Python'
+time_for_one_task = spent_hours / number_of_done_homework
+print ("Курс:",name_of_course,",","всего задач:",number_of_done_homework, ',' " затрачено часов: ",spent_hours,',',"среднее время выполнения:",time_for_one_task,"часа.")
