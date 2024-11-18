7c0f6b104776cdb69300a69e08867cbfce896aae
CheefOfficer <marksmaneld@gmail.com>
2024-11-18 23:58:25 +0300

    homework 3 done

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+name = "Fedor"
+print ("Name:", (name))
+age = 35
+print ("Age:", (age))
+age = [36, 35]
+35 == False
+36 == True
+print ("New age:", (35 and 36))
+"is_student" == True
+print ("isStudent:", bool ("is_student"))
