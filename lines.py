0d2f7e813fda9788f7d5202ae86da411c8dbbd08
CheefOfficer <marksmaneld@gmail.com>
2024-11-20 00:26:50 +0300

    homework 5 done

---
+++ lines.py
@@ -0,0 +1,13 @@
+name = "example"
+print (name[0])
+print (name[-1])
+print (name[2::])
+print (name[::-1])
+print (name[1:7:2])
+print ('--------------')
+name = "Топинамбур"
+print (name[0])
+print (name[-1])
+print (name[5::])
+print (name[::-1])
+print (name[1:10:2])
