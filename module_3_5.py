364f69d1baae09ef82b93c751160c045a7f1e1cd
CheefOfficer <marksmaneld@gmail.com>
2024-12-11 02:27:45 +0300

    я просто не сразу понял что нужно проверку добавить

--- module_3_5.py
+++ module_3_5.py
@@ -1,5 +1,27 @@
+"""""
+def check_zero_T(number):
+    str_number = str(number)
+    s = 0
+    for i in range(len(str_number)):
+        s += i
+        for T in str_number[-1]:
+            if T == "0":
+                str_number = str_number.removesuffix("0")
+                str_number = str_number.__add__("1")
+        return str_number
+"""
+
+
 def get_multiplied_digits(number):
     str_number = str(number)
+    s = 0
+    for i in range(len(str_number)):
+        s += i
+        for T in str_number[-1]:
+            if T == "0":
+                str_number = str_number.removesuffix("0")
+                str_number = str_number.__add__("1")
+
     if len(str_number) > 1:
         first = int(str_number[0])
         return first * get_multiplied_digits(int(str_number[1::]))
@@ -8,6 +30,6 @@ def get_multiplied_digits(number):
 print("--------------------------------------------")
 result = get_multiplied_digits(40203)
 print(result)
-result2 = get_multiplied_digits(4020301)
+result2 = get_multiplied_digits(402030)
 print(result2)
 
