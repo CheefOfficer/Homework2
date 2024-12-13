810dd3cc6bbe5ee4a18680f65c5ae1c4974565a6
CheefOfficer <marksmaneld@gmail.com>
2024-12-14 00:42:56 +0300

    готово

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+def divide(first, second):
+    if second == 0:
+        return "Ошибка"
+    else:
+        return int(first) / int(second)
+if __name__ == '__divide__':
+    divide()
+
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+def divide(first, second):
+    if second == 0:
+        return inf
+    else:
+        return int(first) / int(second)
+if __name__ == '__divide__':
+    divide()
